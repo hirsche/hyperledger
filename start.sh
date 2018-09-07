3c38531b46bf18977bd9f7dec497b323adf14721
Eduard Hirsch <eduard.hirsch@fh-salzburg.ac.at>
2018-09-07 11:25:34 +0200

    temp commit

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+net config server /autodisconnect:-1
 winpty vagrant up --provider hyperv
 #vagrant ssh -c "~/fabric-dev-servers/startFabric.sh"
 IP=`vagrant ssh -c "/sbin/ifconfig eth0 | grep -o '\(inet\ addr:\)[0-9\.]\+'| grep -o '[0-9\.]\+'"`
