5f56fd3fb94393fb53d787d1d8c540e744e21804
Eduard Hirsch <hirsch.eduard@gmail.com>
2018-10-17 21:02:50 +0200

    script simplification

--- start.sh
+++ start.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 vagrant up --provider virtualbox
 #vagrant ssh -c "cd ~/InterlaceBlockchain/chain && ./startFabric.sh"
-IP=`vagrant ssh -c "/sbin/ifconfig eth0 | grep -o '\(inet\ addr:\)[0-9\.]\+'| grep -o '[0-9\.]\+'"`
+#IP=`vagrant ssh -c "/sbin/ifconfig eth0 | grep -o '\(inet\ addr:\)[0-9\.]\+'| grep -o '[0-9\.]\+'"`
 
-if [[ $IP =~ ^[0-9.]+$ ]]
-then
-  vagrant ssh -c "~/runPlayground.sh $IP"
-else
-  echo "Did not receive a valid IPv4 address: $IP"
-fi
+vagrant ssh -c "~/runPlayground.sh"
