d04aa47a759429c5f81b73c7fb5072eb35403c6b
Eduard Hirsch <hirsch.eduard@gmail.com>
2018-10-17 20:53:41 +0200

    simplified scripts

--- scripts/runPlayground.sh
+++ scripts/runPlayground.sh
@@ -2,20 +2,12 @@
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-IP=$1
 PL=../lib/node_modules/composer-playground/cli.js
 
-rm -f nohup.out
 screen -d -m $NVM_BIN/node $NVM_BIN/$PL
 echo "waiting for playground to start ..."
 
-if [[ $IP =~ ^[0-9.]+$ ]]
-then
-  while ! nc -z $IP 8080; do
-    sleep 1 # wait for one second before check again
-  done
-  echo "playground launched."
-else
-  echo "Warning: Did not receive a valid IPv4 address: $IP"  
-  echo "         Payground might not run properly..."
-fi
+while ! nc -z interlace.chain 8080; do
+  sleep 1 # wait for one second before check again
+done
+echo "playground launched."
--- start.sh
+++ start.sh
@@ -2,11 +2,12 @@
 net config server /autodisconnect:-1
 winpty vagrant up --provider hyperv
 #vagrant ssh -c "cd ~/InterlaceBlockchain/chain && ./startFabric.sh"
-IP=`vagrant ssh -c "/sbin/ifconfig eth0 | grep -o '\(inet\ addr:\)[0-9\.]\+'| grep -o '[0-9\.]\+'"`
+#IP=`vagrant ssh -c "/sbin/ifconfig eth0 | grep -o '\(inet\ addr:\)[0-9\.]\+'| grep -o '[0-9\.]\+'"`
 
-if [[ $IP =~ ^[0-9.]+$ ]]
-then
-  vagrant ssh -c "~/runPlayground.sh $IP"
-else
-  echo "Did not receive a valid IPv4 address: $IP"
-fi
+#if [[ $IP =~ ^[0-9.]+$ ]]
+#then
+#  vagrant ssh -c "~/runPlayground.sh $IP"
+#else
+#  echo "Did not receive a valid IPv4 address: $IP"
+#fi
+vagrant ssh -c "~/runPlayground.sh interlace.chain"
